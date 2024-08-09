60885aa16c58deee6760b4ee03cbc6434ce0ec17
anigigauri37 <anigigauri37@gmail.com>
2024-08-09 22:17:45 +0400

    commit

---
+++ level 34/homework/34.py
@@ -0,0 +1,11 @@
+def find_short(s):
+    list1 = s.split(" ")
+
+
+    word_len = len(list1[0])
+    for i in list1:
+        if len(i) < word_len:
+            word_len = len(i)
+    
+    # word_len = 3
+    return word_len
