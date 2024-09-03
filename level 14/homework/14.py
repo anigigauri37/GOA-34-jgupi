c266f5036f6f0f39db5872e21b6a68132f6c38d7
anigigauri37 <anigigauri37@gmail.com>
2024-09-03 19:06:58 +0400

    commit

--- level 14/homework/14.py
+++ level 14/homework/14.py
@@ -1,92 +1,96 @@
-#გაიარეთ while ციკლები და გააკეთეტ 5 მაგალითი while ციკლზე 
-#1
-num1 = int(input("daweret wlovaneba"))
-num2 = 18 
+# #გაიარეთ while ციკლები და გააკეთეტ 5 მაგალითი while ციკლზე 
+# #1
+# num1 = int(input("daweret wlovaneba"))
+# num2 = 18 
 
-while num1 >= num2:
-    print("tqven miiget martvis mowmoba")
-    break
-else:
-    print("samwuxarod ver miiget martvis mowmoba")
-#2
-x = 1 
-y = 101
-while 100 >= x:
-    x += 1
-    y -= 1
-    print(y)
-#3
-num1 = int(input("shemoitanet ricxvi"))
-num2 = 5
+# while num1 >= num2:
+#     print("tqven miiget martvis mowmoba")
+#     break
+# else:
+#     print("samwuxarod ver miiget martvis mowmoba")
+# #2
+# x = 1 
+# y = 101
+# while 100 >= x:
+#     x += 1
+#     y -= 1
+#     print(y)
+# #3
+# num1 = int(input("shemoitanet ricxvi"))
+# num2 = 5
 
-while num1 == num2:
-    print("yochag shen moige")
-    break
-else:
-    print("waage")
+# while num1 == num2:
+#     print("yochag shen moige")
+#     break
+# else:
+#     print("waage")
 
-#გაიარეთ for ციკლი და გააკეთეთ 5 მაგალითი for ციკლებზე
-#1
-for i in range(126, -1, -1):
-    print(i)
-#2
-for i in range(1 , 47):
-    print("1 + ", i, "=", 1 + i)
-#3
-for i in range(10):
-    if i % 2 != 0:
-        print(i)
-#4
-for i in range(30):
-    print("GOA IS THE BEST")
-#5
-for i in range(2,50):
-    i += 4
-    print(i)
+# #გაიარეთ for ციკლი და გააკეთეთ 5 მაგალითი for ციკლებზე
+# #1
+# for i in range(126, -1, -1):
+#     print(i)
+# #2
+# for i in range(1 , 47):
+#     print("1 + ", i, "=", 1 + i)
+# #3
+# for i in range(10):
+#     if i % 2 != 0:
+#         print(i)
+# #4
+# for i in range(30):
+#     print("GOA IS THE BEST")
+# #5
+# for i in range(2,50):
+#     i += 4
+#     print(i)
 
-#გააკეთეთ 5 მაგალითი if-else
+# #გააკეთეთ 5 მაგალითი if-else
 
-#1
-height = int(input(" Enter Your Height: "))
-weight = int(input(" Enter Your Weight: "))
+# #1
+# height = int(input(" Enter Your Height: "))
+# weight = int(input(" Enter Your Weight: "))
 
-height_metres = height / 100.0
+# height_metres = height / 100.0
 
-bmi = weight / (height ** 2)
+# bmi = weight / (height ** 2)
 
-if bmi <18.5:
-    bmi_category = "underweight"
+# if bmi <18.5:
+#     bmi_category = "underweight"
 
-elif bmi < 25:
-    bmi_category = "Normal weight"
+# elif bmi < 25:
+#     bmi_category = "Normal weight"
 
-elif bmi < 30:
-    bmi_category = "Overweight"
+# elif bmi < 30:
+#     bmi_category = "Overweight"
 
-else:
-    bmi_category = "Obesity"
+# else:
+#     bmi_category = "Obesity"
 
-print(f"Your BMI is: {bmi:.2f}")
-print(f"You are classified as: {bmi_category}")
-#2
-user_input1 = int(input("enter your age"))
-if user_input1 < 60:
-    print("tqven ver gamoiyenebt pasdaklebas: ")
-else: 
-    print("shen shegidzlia gamoiyeno pasdakleba: ")
-user_input2 = int(input("enter your price: "))
-if user_input2 > 100:
-    print("tqven gaqvt gamoiyenot upro magali pasdakleba: ")
-else:
-    print("tqven gaqvt gamoiyenot chveulebrivi pasdakleba: ")
-#3
-user_input3 = int(input("enter your gpa"))
-if user_input3 < 3.0:
-    print("shen shegidzlia miigo stipendia: ")
-else:
-    print("shen ar sheidzlia miigo stipendia: ")
-user_input4 = int(input("enter your incomo: "))
-if user_input4 < 50000:
-    print("shen shegidzlia miigo umaglesi stipendia: ")
-else: 
-    print("shen ar shegidzlia miigo umaglesi stipendia: ")
+# print(f"Your BMI is: {bmi:.2f}")
+# print(f"You are classified as: {bmi_category}")
+# #2
+# user_input1 = int(input("enter your age"))
+# if user_input1 < 60:
+#     print("tqven ver gamoiyenebt pasdaklebas: ")
+# else: 
+#     print("shen shegidzlia gamoiyeno pasdakleba: ")
+# user_input2 = int(input("enter your price: "))
+# if user_input2 > 100:
+#     print("tqven gaqvt gamoiyenot upro magali pasdakleba: ")
+# else:
+#     print("tqven gaqvt gamoiyenot chveulebrivi pasdakleba: ")
+# #3
+# user_input3 = int(input("enter your gpa"))
+# if user_input3 < 3.0:
+#     print("shen shegidzlia miigo stipendia: ")
+# else:
+#     print("shen ar sheidzlia miigo stipendia: ")
+# user_input4 = int(input("enter your incomo: "))
+# if user_input4 < 50000:
+#     print("shen shegidzlia miigo umaglesi stipendia: ")
+# else: 
+#     print("shen ar shegidzlia miigo umaglesi stipendia: ")
+
+
+
+
