fd051414e0965e6da4572a06bdca4acdedd705b9
anigigauri37 <anigigauri37@gmail.com>
2024-09-20 20:51:00 +0400

    commit

---
+++ level 57/homework/57.js
@@ -0,0 +1,145 @@
+// 1
+
+greet = "Hello, world!";
+console.log(greet)
+
+// 2
+
+let counter = 10;
+console.log(counter)
+
+// 3
+
+counter = 10;
+console.log(counter)
+
+// 4
+
+const birthYear = 2010;
+console.log(birthYear)
+
+// 5
+
+let x = 10
+let y = 15
+console.log(x + y)
+
+// 6
+
+const firstName = "anni";
+console.log(firstName)
+
+// 7
+
+let temperature = 25;
+console.log(temperature)
+
+// 8
+
+const isAdult = "true";
+console.log(isAdult)
+
+// 9
+
+let age = 14;
+console.log(age)
+
+age = 15;
+console.log(age)
+
+// 10
+
+const PI = 3.14159;
+console.log(PI)
+
+// 11
+
+let a = 5
+let b = 3
+console.log(a + b)
+
+// 12
+
+let num1 = 2
+let num2 = 5
+console.log(num1 * num2)
+
+// 13
+
+let m = 26
+let n = 2
+console.log(m / n)
+
+// 14
+
+let d = 15
+let e = 8
+console.log(d - e)
+
+// 15
+
+let num = 10 % 4;
+console.log(num)
+
+// 16
+
+let nums = 100;
+nums += 200
+console.log(nums)
+
+// 17
+
+let ages = (20 % 4) / 10;
+console.log(ages)
+
+// 18
+
+let height = 1
+let width = 2
+let numbers = height * width
+console.log(numbers)
+
+// 19
+
+let square = 6 ** 3;
+console.log(square)
+
+// 20
+
+let name = (100 % 50) / 2;
+console.log(name)
+
+// 21
+
+let l = 20
+let w = 15
+let log = l > w
+console.log(log)
+
+// 22
+
+let r = 10
+let o = 10
+let is = a === b
+console.log(is)
+
+// 23
+
+let age1 = 18 
+let age2 = 20
+let int = age1 < age2
+console.log(int)
+
+// 24
+
+let result = 5 <= 10
+console.log(result)
+
+// 25
+
+
+let c = 14
+let j = 55
+let str = c !== j
+console.log(str)
+
