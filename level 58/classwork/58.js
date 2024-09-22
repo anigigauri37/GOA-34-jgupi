b3a9837e7aaef0db0b5ecf4cafb078c46826ffe8
anigigauri37 <anigigauri37@gmail.com>
2024-09-22 17:17:50 +0400

    commit

---
+++ level 58/classwork/58.js
@@ -0,0 +1,12 @@
+const Harry Potter = {
+    person: "harry potter",
+    height: 1.70,
+    Location: 'Hogwarts',
+}
+
+harry_potter.year = 2000
+
+delete harry_potter.Location
+
+console.log(Harry Potter)
+
---
+++ level 58/homework/58.js
@@ -0,0 +1,18 @@
+const car = {
+    brend: "BMW",
+    model: 'm5',
+    year: 2007,
+    color: 'red',
+}
+
+car.color = 'bleck'
+
+delete car.model
+
+let startEngine = 'Engine stardet'
+console.log(startEngine)
+
+console.log(car.brend)
+console.log(car.year)
+console.log(car)
+
