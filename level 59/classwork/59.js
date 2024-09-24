2c1cd2a2c59f993f8c645898bde22d8e262d81a4
anigigauri37 <anigigauri37@gmail.com>
2024-09-24 19:37:16 +0400

    commit

---
+++ level 59/classwork/59.js
@@ -0,0 +1,12 @@
+const first = document.getElementById("text1");
+const second = document.getElementById("text2");
+const third = document.getElementById("text3");
+
+first.textContent = "good"
+
+first.style.color = "red";
+second.style.fontSize = '50px';
+third.style.fontSize = "30px"
+
+
+
