c3f9d30eaa91bf6ce4ae4dcf6b7519c3dceb9344
anigigauri37 <anigigauri37@gmail.com>
2024-09-30 22:55:05 +0400

    commit

---
+++ level 62/classwork/prc.js
@@ -0,0 +1,4 @@
+// ჩამოწერეთ ყველა თქვენთვის ცნობილი კარგი პრაქტიკა და განიხილეთ თუ რას მოიცავენ ისინი
+
+// komentarebi - komentarebis axsna gvexmareba, sataurebistvis an kodi aaxsnelad
+// shecdoma, eror 
