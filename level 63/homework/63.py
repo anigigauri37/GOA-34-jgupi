75e8bac19aec36d39499f63c24c26f1d442d5eb1
anigigauri37 <anigigauri37@gmail.com>
2024-10-03 21:00:07 +0400

    commit

---
+++ level 63/homework/63.py
@@ -0,0 +1,4 @@
+#Greeting Function Create a function greet that accepts a name as a parameter and greets the person. If no name is provided, the default name should be "Guest".
+
+def greet(name="Guest"):
+    print(f"Hello, {name}!")
